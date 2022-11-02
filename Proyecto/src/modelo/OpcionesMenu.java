c33738951482a1e9fd1f81ad6dda32b0edfbb959
maris <maris@LAPTOP-JORGEZT>
2022-11-01 21:34:07 -0600

    primeras lineas opciones menu

---
+++ Proyecto/src/modelo/OpcionesMenu.java
@@ -0,0 +1,40 @@
+/**
+ * Esta clase contiene las funciones del menu
+ * @author Keiron Garro Matamoros C23212, Jorge Zúñiga Torres C08740, Jeaustin Castro López C21912
+ */
+
+package modelo;
+import vistas.terminal.Escritor;
+import vistas.terminal.Lector;
+
+public class OpcionesMenu {
+    Escritor escritor = new Escritor();
+    Lector lector = new Lector();
+    
+    public OpcionesMenu(){
+    
+    }
+    
+    public void insertarPregunta(String texto, String categoria, int tipoPregunta){
+        //Texto de la pregunta
+        
+        
+    }
+    
+    public void mostrarPregunta(){
+        
+    }
+    
+    public void actualizarPregunta(){
+        
+    }
+    
+    public void eliminarPregunta(){
+        
+    }
+    
+    public void salir(){
+        
+    }
+    
+}
