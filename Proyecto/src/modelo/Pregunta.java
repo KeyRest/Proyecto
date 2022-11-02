c6b8ec85aa2e4f236496ad279494a403c9d80986
Jeaustin-Castro <justincastrolo32@gmail.com>
2022-11-02 17:45:11 -0600

    Codigo pregunta

--- Proyecto/src/modelo/Pregunta.java
+++ Proyecto/src/modelo/Pregunta.java
@@ -1,13 +1,89 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
 /**
- *
- * @author maris
+ * @author Keiron Garro Matamoros C23212, Jorge Zúñiga Torres C08740, Jeaustin
+ * Castro López C21912
+ * @version 1/11/2022
  */
-public class Pregunta {
+public abstract class Pregunta {
+
+    // Variables
+    private String textoPregunta;
+    private String categoria;
+    private int tipoPregunta;
+    private static int id;
     
+    private final int VERDADERO_FALSO = 0;
+    private final int SELECCION_UNICA = 1;
+    private final int SELECCION_MULTIPLE = 2;
+    
+    //Constructores
+    public Pregunta(String textoPregunta, String categoria, int tipoPregunta, int id) {
+        this.textoPregunta = textoPregunta;
+        this.categoria = categoria;
+        this.tipoPregunta = tipoPregunta;
+        this.id = id;
+    }
+
+    public Pregunta() {
+        this.textoPregunta = "";
+        this.categoria = "";
+        this.tipoPregunta = 0;
+        this.id = 0;
+    }
+    // Getters
+
+    public String getTextoPregunta() {
+        return textoPregunta;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public int getTipoPregunta() {
+        return tipoPregunta;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    // Setters
+    public void setTexto(String textoPregunta) {
+        this.textoPregunta = textoPregunta;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setTipoPregunta(int tipoPregunta) {
+        this.tipoPregunta = tipoPregunta;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getVERDADERO_FALSO() {
+        return VERDADERO_FALSO;
+    }
+
+    public int getSELECCION_UNICA() {
+        return SELECCION_UNICA;
+    }
+
+    public int getSELECCION_MULTIPLE() {
+        return SELECCION_MULTIPLE;
+    }
+    
+   
+
+    public abstract void toFileString();
+
+    @Override
+    public String toString() {
+        return "Pregunta{" + "textoPregunta=" + textoPregunta + ", categoria=" + categoria + ", tipoPregunta=" + tipoPregunta + ", id=" + id + '}';
+    }
 }
